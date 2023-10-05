eda728e73f95bf540b267304519fc083bb2622d5
Chukwuemeka Kennedy <princeukaga@gmail.com>
2023-10-05 14:32:52 +0100

    updated home page

---
+++ lib/util/login_button.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class LoginButton extends StatelessWidget {
+  const LoginButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
+      elevation: 10,
+      child: Container(
+        height: 50,
+        width: 273,
+        decoration: BoxDecoration(
+          color: const Color(0xFF229397),
+          borderRadius: BorderRadius.circular(30),
+        ),
+        child: const Center(
+          child: Text(
+            'Login',
+            style: TextStyle(fontSize: 18, color: Colors.white),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/util/user_text_field.dart
+++ lib/util/user_text_field.dart
@@ -11,12 +11,25 @@ class UserTextField extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return TextField(
-      decoration: InputDecoration(
-        border: const OutlineInputBorder(),
-        labelText: label,
-        labelStyle: TextStyle(color: Colors.grey, fontWeight: FontWeight.bold),
-        icon: Icon(icon),
+    return Padding(
+      padding: const EdgeInsets.all(5.0),
+      child: Container(
+        height: 45,
+        width: 273,
+        decoration: const BoxDecoration(
+            borderRadius: BorderRadius.only(
+          bottomLeft: Radius.circular(15),
+          bottomRight: Radius.circular(15),
+        )),
+        child: TextField(
+          decoration: InputDecoration(
+            border: const OutlineInputBorder(),
+            labelText: label,
+            labelStyle: const TextStyle(
+                color: Colors.grey, fontWeight: FontWeight.bold),
+            prefixIcon: Icon(icon),
+          ),
+        ),
       ),
     );
   }
